88f0262e8050b49545889a377d453d73799d231c
SUNKARI KRISHNAVENI <89072368+Krishnaveni-02@users.noreply.github.com>
2022-11-10 23:03:45 +0530

    Mapped url

--- src/main/java/com/krishnaveni/ass5/SkCiCdApplication.java
+++ src/main/java/com/krishnaveni/ass5/SkCiCdApplication.java
@@ -2,12 +2,14 @@ package com.krishnaveni.ass5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SkCiCdApplication {
 	
+	@GetMapping("/execute")
 	public String executeCICD() {
 		return "You have successfully implemented CI/CD Pipeline";
 	}
